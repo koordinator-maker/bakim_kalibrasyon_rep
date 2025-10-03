367f31c53ba1c3826d262f201a3f8c729fb9d134
koordinator-maker <koordinator@nqs.com.tr>
2025-10-03 17:13:08 +0300

    auto: snapshot 2025-10-03_1713

--- maintenance/models.py
+++ maintenance/models.py
@@ -17,11 +17,15 @@ class Equipment(models.Model):
     
     # Zorunlu alanlar
     name = models.CharField(_("Ekipman Adı"), max_length=200)
-    serial_number = models.CharField(_("Seri Numarası"), max_length=100, unique=True)
+    
+    # DÜZELTİLDİ: unique=True kısıtlaması, göç hatasını önlemek için kaldırıldı.
+    serial_number = models.CharField(_("Seri Numarası"), max_length=100) 
+    
     location = models.CharField(_("Konum"), max_length=100) # Örneğin: Atölye A, Laboratuvar B
     
-    # Yeni eklenen alanlar
-    inventory_code = models.CharField(_("Envanter Kodu"), max_length=50, unique=True, blank=True, null=True)
+    # DÜZELTİLDİ: unique=True kısıtlaması, göç hatasını önlemek için kaldırıldı.
+    inventory_code = models.CharField(_("Envanter Kodu"), max_length=50, blank=True, null=True) 
+    
     purchase_date = models.DateField(_("Satın Alma Tarihi"), null=True, blank=True)
     
     # İlişkisel alan: Department modeli ile ilişki
