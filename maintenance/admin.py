6ac7bd4de7be45ff57798ee96b8cce6e92a9dc28
koordinator-maker <koordinator@nqs.com.tr>
2025-10-05 18:13:07 +0300

    auto: snapshot 2025-10-05_1813

--- maintenance/admin.py
+++ maintenance/admin.py
@@ -3,9 +3,5 @@ from .models import Equipment
 
 @admin.register(Equipment)
 class EquipmentAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "serial_number")  # sadece kesin olanlar
+    list_display = ("id", "name", "serial_number")
     search_fields = ("name", "serial_number")
-    # Geçici olarak filtre yok (hata çıkaranları kaldırdık)
-    # list_filter = ()
-    def has_add_permission(self, request):
-        return True
