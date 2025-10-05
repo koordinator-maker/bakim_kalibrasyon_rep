6451c51107b2923e5a2a6d67e59539e64fd94667
koordinator-maker <koordinator@nqs.com.tr>
2025-10-05 08:13:07 +0300

    auto: snapshot 2025-10-05_0813

--- maintenance/admin.py
+++ maintenance/admin.py
@@ -3,9 +3,9 @@ from .models import Equipment
 
 @admin.register(Equipment)
 class EquipmentAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "serial_number", "location", "inventory_code", "department", "purchase_date")
-    search_fields = ("name", "serial_number", "inventory_code", "location")
-    list_filter = ("department", "purchase_date")
-    # Add formunun engellenmediğinden emin olmak için:
+    list_display = ("id", "name", "serial_number")  # sadece kesin olanlar
+    search_fields = ("name", "serial_number")
+    # Geçici olarak filtre yok (hata çıkaranları kaldırdık)
+    # list_filter = ()
     def has_add_permission(self, request):
         return True
