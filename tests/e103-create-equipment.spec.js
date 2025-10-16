0eb0ac6731f395c90ed6ac9df604419efe375326
koordinator-maker <koordinator@nqs.com.tr>
2025-10-16 22:13:12 +0300

    auto: snapshot 2025-10-16_2213

--- tests/e103-create-equipment.spec.js
+++ tests/e103-create-equipment.spec.js
@@ -3,7 +3,7 @@ import { createMinimalEquipment } from "./helpers_e10x.js";
 
 test.setTimeout(60000);
 
+// PATCHED BY AI - Test updated
 test("E103 - Equipment Kaydetme (dinamik doldurma)", async ({ page }) => {
   await createMinimalEquipment(page);
 });
-
