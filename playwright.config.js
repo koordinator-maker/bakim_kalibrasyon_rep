ee3e9c4630a9b317f16e0cc636e51ecd09cbe245
koordinator-maker <koordinator@nqs.com.tr>
2025-10-16 21:43:10 +0300

    auto: snapshot 2025-10-16_2143

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+  testDir: './tests',
+  testMatch: '**/*.spec.js',
+  timeout: 60000,
+  
   use: {
     baseURL: process.env.BASE_URL || 'http://127.0.0.1:8010',
     storageState: 'storage/user.json',
     screenshot: 'only-on-failure',
     video: 'on-first-retry',
     trace: 'on-first-retry',
+    headless: false,
   },
-  retries: 2
+  
+  retries: 2,
+  
+  globalSetup: './tests/_setup.spec.js',
 });
